40d8a801291487fa2e15c2d774b747198d94c552
Derek <d.halman@gmail.com>
2020-05-20 19:59:09 -0700

    gomu: Deploy local changes

--- com/git.go
+++ com/git.go
@@ -45,8 +45,7 @@ func (file *FileWrapper) CheckoutOrCreateBranch(branch string) (switched, create
 
 // Fetch calls git fetch in provided dir
 func (file *FileWrapper) Fetch() (err error) {
-	file.RunCmd("git", "fetch")
-	return file.RunCmd("git", "fetch", "--tags", "--force")
+	return file.RunCmd("git", "fetch", "--prune", "--prune-tags")
 }
 
 // Merge merges other branch into current branch
